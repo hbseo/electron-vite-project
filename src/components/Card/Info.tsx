6f3318bfeb1d5b0862daded363298d3b7283c955
hbseo <gusqja8753@gmail.com>
2023-04-07 17:29:40 +0900

    Add overflow text tooltip

--- src/components/Card/Info.tsx
+++ src/components/Card/Info.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { Card, CardBody, Stack } from '@chakra-ui/react';
-
-const StyledInfoWrapper = styled(Card)`
-    padding: 10px;
-`;
-const StyledInfoStack = styled(Stack)``;
+import { Box, Card, CardBody, Center, Flex, Text } from '@chakra-ui/react';
+import { OverflownText } from '@/custom/OverflownText';
 
 export function Info() {
     return (
-        <StyledInfoWrapper>
-            <CardBody>
-                <StyledInfoStack></StyledInfoStack>
+        <Card>
+            <CardBody padding={'10px'}>
+                <Flex>
+                    <Box width={'25%'} textAlign={'left'}>
+                        <Text fontSize={'2xs'}>버스 번호</Text>
+                        <Text fontSize={'sm'}>5714번</Text>
+                    </Box>
+                    <Box width={'50%'}>
+                        <Center fontSize={'2xs'}>현재 위치</Center>
+                        <Center>
+                            <OverflownText noOfLines={1} fontSize={'sm'}>
+                                대구의료원라파엘웰빙센터건너
+                            </OverflownText>
+                        </Center>
+                    </Box>
+                    <Box width={'25%'} textAlign={'right'}>
+                        <Text fontSize={'2xs'}>남은 시간</Text>
+                        <Text fontSize={'sm'}>12분</Text>
+                    </Box>
+                </Flex>
             </CardBody>
-        </StyledInfoWrapper>
+        </Card>
     );
 }
