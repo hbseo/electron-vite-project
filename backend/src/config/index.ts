e6708e1b79442a62670c6dcef7ce8892e8ab976b
hbseo <gusqja8753@gmail.com>
2023-04-20 17:56:42 +0900

    Add nodemon.json, jest.config.js

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,3 +3,4 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT } = process.env;
+export const { API_KEY } = process.env;
