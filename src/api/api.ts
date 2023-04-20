f621e0f1374d4b6a0327dc4c77df5c8a9fe76ed4
hbseo <gusqja8753@gmail.com>
2023-04-20 14:10:29 +0900

    Remove http-proxy-middleware
    using vite.config.ts proxy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,16 @@
-const VITE_DATA_API_KEY = import.meta.env;
+const { VITE_DATA_API_KEY } = import.meta.env;
 
 export async function getBusData() {
     return fetch('./data/busData.json').then((response) => response.json());
 }
 
 export async function getBusArrivalList(arsId: string | number) {
-    return fetch(`/api/stationinfo/getStationByUid?serviceKey=${VITE_DATA_API_KEY}&arsId=${arsId}`, {
+    console.log(VITE_DATA_API_KEY);
+    return fetch(`/api/stationinfo/getStationByUid?serviceKey=${VITE_DATA_API_KEY}&arsId=${arsId}&resultType=json`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
         },
-    }).then((response) => console.log(response));
+    }).then((response) => response.json());
 }
