da84e504ae0622ee4b7b5e7e81f4f8f43772e94d
hbseo <gusqja8753@gmail.com>
2023-05-08 13:59:29 +0900

    change bus arrival component

--- frontend/src/components/Bus/BusArrival.tsx
+++ frontend/src/components/Bus/BusArrival.tsx
@@ -1,46 +1,37 @@
 import React from 'react';
-import { Text, Flex } from '@chakra-ui/react';
+import { Text, Flex, Center, FlexProps } from '@chakra-ui/react';
 
-export interface BusArrivalProps {
+export interface BusArrivalProps extends FlexProps {
     name: string;
     arrmsg1: string;
     arrmsg2?: string;
+    show?: boolean;
 }
 
-export function BusArrival({ name, arrmsg1, arrmsg2 }: React.PropsWithChildren<BusArrivalProps>) {
+export function BusArrival({
+    name,
+    arrmsg1,
+    arrmsg2,
+    show = false,
+    ...rest
+}: React.PropsWithChildren<BusArrivalProps>) {
     return (
-        <Flex
-            direction={'column'}
-            borderTop={'1px solid black'}
-            borderLeft={'1px solid black'}
-            borderRadius={'md'}
-            w={'80px'}
-        >
-            <Text fontSize={'lg'} as={'b'} borderRight={'1px solid black'} borderTopRadius={'md'}>
-                5152
-            </Text>
-            <Flex>
-                <Text
-                    fontSize={'2xs'}
-                    noOfLines={1}
-                    borderBottom={'1px solid black'}
-                    borderRight={'1px solid black'}
-                    borderBottomRadius={'md'}
-                    w={'40%'}
-                >
-                    곧도착
+        <Flex direction={'column'} w={'80px'} {...rest}>
+            <Center>
+                <Text fontSize={'md'} as={'b'}>
+                    {name}
                 </Text>
-                <Text
-                    fontSize={'2xs'}
-                    noOfLines={1}
-                    borderTop={'1px solid black'}
-                    borderLeft={'1px solid transparent'}
-                    borderRightRadius={0}
-                    w={'60%'}
-                >
-                    다음 25분
+            </Center>
+            <Center>
+                <Text fontSize={'2xs'} noOfLines={1}>
+                    {arrmsg1}
                 </Text>
-            </Flex>
+                {show && arrmsg2 ? (
+                    <Text fontSize={'2xs'} noOfLines={1} align={'right'}>
+                        / {arrmsg2}
+                    </Text>
+                ) : null}
+            </Center>
         </Flex>
     );
 }
