bcffc695cfc8e3130304a3c44a3f07405c3da4b5
hbseo <gusqja8753@gmail.com>
2023-09-21 16:43:25 +0900

    clean code modal

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useRef } from 'react';
 import { classNames } from '@/utils/common';
-import autoAnimate from '@formkit/auto-animate';
 
 export const Modal = ({
   open,
@@ -17,28 +15,13 @@ export const Modal = ({
   className?: string;
   children?: React.ReactNode;
 }) => {
-  const container = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    container.current && autoAnimate(container.current);
-  }, [container]);
-
-  useEffect(() => {
-    if (!container.current) return;
-
-    if (open) {
-      container.current.style.setProperty('visibility', 'visible');
-      container.current.style.setProperty('opacity', '1');
-    } else {
-      container.current.style.setProperty('visibility', 'hidden');
-      container.current.style.setProperty('opacity', '0');
-    }
-  }, [open]);
-
   return (
     <div
-      className="absolute left-0 top-0 z-50 h-screen w-screen bg-[rgba(0,0,0,0.5)] transition-opacity duration-300"
-      style={{ visibility: `${open ? 'visible' : 'hidden'}` }}
+      className="absolute left-0 top-0 z-50 h-screen w-screen bg-[rgba(0,0,0,0.5)] transition-all duration-300"
+      style={{
+        visibility: `${open ? 'visible' : 'hidden'}`,
+        opacity: `${open ? '1' : '0'}`
+      }}
       data-scope="outer"
       onClick={(e: React.MouseEvent) => {
         if (!(e.target instanceof HTMLDivElement)) {
@@ -48,7 +31,6 @@ export const Modal = ({
           setOpen(false);
         }
       }}
-      ref={container}
     >
       <div
         className={classNames(
