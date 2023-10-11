a9b24ed211c56e7118f1473dfca9506ee5d028da
hbseo <gusqja8753@gmail.com>
2023-10-11 13:38:15 +0900

    improve calendar component
    styling, action

--- src/components/common/Calendar.tsx
+++ src/components/common/Calendar.tsx
@@ -1,22 +1,25 @@
 import { classNames } from '@/utils/common';
-import { useCallback } from 'react';
-
-export type CalendarDataType = {
-  [key: number]: string;
-};
+import { useCallback, useState } from 'react';
 
 export const Calendar = ({
-  data,
   month,
-  className
+  className,
+  action
 }: {
-  data?: CalendarDataType;
   month: Date;
   className?: string;
+  action?: ({
+    year,
+    month,
+    day
+  }: {
+    year: number;
+    month: number;
+    day: number;
+  }) => void;
 }) => {
   const currentYear = month.getFullYear();
   const currentMonth = month.getMonth();
-  const currentDate = month.getDate();
 
   const startDay = new Date(currentYear, currentMonth, 0);
   const prevDate = startDay.getDate();
@@ -24,16 +27,24 @@ export const Calendar = ({
 
   const endDay = new Date(currentYear, currentMonth + 1, 0);
   const nextDate = endDay.getDate();
-  const nextDay = endDay.getDay();
+  // const nextDay = endDay.getDay();
 
-  console.log(
-    `prevDate: ${prevDate} prevDay: ${prevDay} nextDate: ${nextDate} nextDay: ${nextDay}`
+  const [selectedDate, setSelectedDate] = useState<number>(1);
+
+  const handleClickDate = useCallback(
+    (date: number) => {
+      setSelectedDate(date);
+      if (action) {
+        action({ year: currentYear, month: currentMonth + 1, day: date });
+      }
+    },
+    [action, currentYear, currentMonth]
   );
 
   const makePrevDate = useCallback(() => {
     const result = [];
     for (let i = prevDate - prevDay; i <= prevDate; i++) {
-      result.push(<div key={i}>{i}</div>);
+      result.push(<div key={i}></div>);
     }
     return result;
   }, [prevDate, prevDay]);
@@ -41,34 +52,47 @@ export const Calendar = ({
   const makeNowDate = useCallback(() => {
     const result = [];
     for (let i = 1; i <= nextDate; i++) {
-      result.push(<div key={i}>{i}</div>);
+      result.push(
+        <div
+          key={i}
+          className={classNames(
+            'rounded-md duration-200 hover:bg-slate-200 active:bg-slate-400',
+            i === selectedDate && 'bg-slate-200'
+          )}
+          onClick={() => {
+            handleClickDate(i);
+          }}
+        >
+          {i}
+        </div>
+      );
     }
     return result;
-  }, [nextDate]);
+  }, [nextDate, selectedDate, handleClickDate]);
 
-  const makeNextDate = useCallback(() => {
-    const result = [];
-    for (let i = 1; i <= 6 - nextDay; i++) {
-      result.push(<div key={i}>{i}</div>);
-    }
-    return result;
-  }, [nextDay]);
+  // const makeNextDate = useCallback(() => {
+  //   const result = [];
+  //   for (let i = 1; i <= 6 - nextDay; i++) {
+  //     result.push(<div key={i}></div>);
+  //   }
+  //   return result;
+  // }, [nextDay]);
 
   return (
     <div className={classNames(className)}>
-      <ul>
-        <li>일</li>
+      <ul className="mb-1 flex justify-around font-bold">
+        <li className="text-red-600">일</li>
         <li>월</li>
         <li>화</li>
         <li>수</li>
         <li>목</li>
         <li>금</li>
-        <li>토</li>
+        <li className="text-blue-700">토</li>
       </ul>
-      <div>
+      <div className="grid grid-cols-7 gap-1 text-center">
         {makePrevDate()}
         {makeNowDate()}
-        {makeNextDate()}
+        {/* {makeNextDate()} */}
       </div>
     </div>
   );
--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -12,10 +12,22 @@ export const Page = () => {
     today.getMonth(),
     today.getDate()
   );
+
+  const test = ({
+    year,
+    month,
+    day
+  }: {
+    year: number;
+    month: number;
+    day: number;
+  }) => {
+    console.log(year, month, day);
+  };
   return (
     <Layout>
       <h1 className="text-xl font-bold">일정</h1>
-      <Calendar month={thisMonth} />
+      <Calendar month={thisMonth} action={test} />
     </Layout>
   );
 };
